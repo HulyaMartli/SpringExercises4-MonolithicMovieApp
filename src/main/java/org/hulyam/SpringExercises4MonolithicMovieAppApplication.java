3006b1a95edd51e58332a1a0c08e9a9780e657e0
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-08-13 07:33:44 +0300

    Initial commit

---
+++ src/main/java/org/hulyam/SpringExercises4MonolithicMovieAppApplication.java
@@ -0,0 +1,13 @@
+package org.hulyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExercises4MonolithicMovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExercises4MonolithicMovieAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hulyam/SpringExercises4MonolithicMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hulyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExercises4MonolithicMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
